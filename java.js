b853cd7f9661b61404c40d2331ca0da40e4860f4
Fred65n6 <79079101+Fred65n6@users.noreply.github.com>
2021-06-08 11:54:27 +0200

    ...

--- java.js
+++ java.js
@@ -2,4 +2,18 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
   console.log("siden vises");
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
+
+function toggleMenu() {
+  console.log("toggleMenu");
+  document.querySelector("#menu").classList.toggle("hidden");
+
+  let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+}
+//   if (erskjult == true) {
+//     document.querySelector("#menuknap").textContent = "☰";
+//   } else {
+//     document.querySelector("#menuknap").textContent = "X";
+//   }
+// }
